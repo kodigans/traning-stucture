e66693921c8b0f69fb9afc942aa53cd48f2aa0cd
Artem Balabanov <artem_balabanov@epam.com>
2019-01-24 12:58:20 +0300

    add rule for *.less in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader'],
+        }),
+      },
+    ],
+  },
+  plugins: [new ExtractTextPlugin('style/main.css')],
 };
