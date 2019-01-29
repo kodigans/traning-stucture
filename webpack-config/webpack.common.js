fab74ccfb5baf26f5553924e15d2e96e3ff52c4c
Artem Balabanov <artem_balabanov@epam.com>
2019-01-29 14:37:44 +0300

    change webpack module

--- webpack-config/webpack.common.js
+++ webpack-config/webpack.common.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: { main: './src/index.js'},
     output: {        
-      path: path.resolve(__dirname, 'build'),
-      filename: 'main.js'
+      path: path.resolve(__dirname, '../build'),
+      filename: 'bundle.js'
     }, 
-}
+};
---
+++ webpack-config/webpack.production.js
@@ -0,0 +1,30 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'less-loader'],
+                }),
+            },
+            {
+                test: /\.(svg|png|woff|ttf|eot|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[folder]/[name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+
+    plugins: [
+        new ExtractTextPlugin('style/main.css'),
+    ]
+};
