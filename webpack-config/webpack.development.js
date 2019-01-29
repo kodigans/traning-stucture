ce5d9a362eee1c5b0ef756871abf7066e357ef2c
Who am i <help@me>
2019-01-29 13:15:27 +0300

    in progress

---
+++ webpack-config/webpack.development.js
@@ -0,0 +1,38 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devServer: {            
+        open: true
+      },
+      module: {
+        rules: [
+          {
+            test: /\.less$/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: ['css-loader', 'less-loader'],
+            }),
+          },
+          {
+            test: /\.(svg|png|woff|ttf|eot|woff2)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[folder]/[name].[ext]',
+                },
+              },
+            ],
+          },
+        ],
+      },
+    
+      plugins: [
+        new ExtractTextPlugin('style/main.css'),
+        new HtmlWebpackPlugin({
+          template: './src/index.html',      
+        }),        
+      ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,53 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack-config/webpack.common');
+const build = require('./webpack-config/webpack.development');
+const prod = require('./webpack-config/webpack.production');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
+console.log(process.env.qwe);
+module.exports = env => { 
+  console.log(env.production);
+  return merge(common, build) };
 
-  module: {
-    rules: [
-      {
-        test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'less-loader'],
-        }),
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[folder]/[name].[ext]',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [new ExtractTextPlugin('style/main.css')],
-};
+// const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {    
+//   entry: { main: './src/index.js'},
+//   output: {        
+//     path: path.resolve(__dirname, 'build'),
+//     filename: 'main.js'
+//   }, 
+//   devServer: {            
+//     open: true
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.less$/,
+//         use: ExtractTextPlugin.extract({
+//           fallback: 'style-loader',
+//           use: ['css-loader', 'less-loader'],
+//         }),
+//       },
+//       {
+//         test: /\.(svg|png|woff|ttf|eot|woff2)$/,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//             options: {
+//               name: '[folder]/[name].[ext]',
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
+
+//   plugins: [
+//     new ExtractTextPlugin('style/main.css'),
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',      
+//     }),        
+//   ]
+// }
