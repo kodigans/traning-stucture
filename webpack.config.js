e3ef63f9fcdb81351d718aca92be5fe8c9d8a5c7
Artem Balabanov <artem_balabanov@epam.com>
2019-01-29 14:33:43 +0300

    change webpack.config structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,10 @@ const common = require('./webpack-config/webpack.common');
 const build = require('./webpack-config/webpack.development');
 const prod = require('./webpack-config/webpack.production');
 
-console.log(process.env.qwe);
-module.exports = env => { 
-  console.log(env.production);
-  return merge(common, build) };
-
-// const path = require('path');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {    
-//   entry: { main: './src/index.js'},
-//   output: {        
-//     path: path.resolve(__dirname, 'build'),
-//     filename: 'main.js'
-//   }, 
-//   devServer: {            
-//     open: true
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.less$/,
-//         use: ExtractTextPlugin.extract({
-//           fallback: 'style-loader',
-//           use: ['css-loader', 'less-loader'],
-//         }),
-//       },
-//       {
-//         test: /\.(svg|png|woff|ttf|eot|woff2)$/,
-//         use: [
-//           {
-//             loader: 'file-loader',
-//             options: {
-//               name: '[folder]/[name].[ext]',
-//             },
-//           },
-//         ],
-//       },
-//     ],
-//   },
-
-//   plugins: [
-//     new ExtractTextPlugin('style/main.css'),
-//     new HtmlWebpackPlugin({
-//       template: './src/index.html',      
-//     }),        
-//   ]
-// }
+module.exports = env => {
+    if (env.production) {
+        return merge(common, build);
+    } else {
+        return merge(common, prod)
+    }
+};
